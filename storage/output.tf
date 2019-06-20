c1d517fb5423463027836962bea643a33c02e43a
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-06-20 19:45:21 +1000

    get access key

--- storage/output.tf
+++ storage/output.tf
@@ -1,3 +1,3 @@
 output "storage_access_key" {
-  value = "${azurerm_container_registry.container_registry.primary_access_key}"
+  value = "${azurerm_container_registry.azurerm_storage_account.primary_access_key}"
 }
