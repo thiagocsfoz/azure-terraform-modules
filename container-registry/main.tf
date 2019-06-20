3bd633cc3bc5cf59cea62e781684204570fab189
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-06-21 09:23:49 +1000

    output username and password

--- container-registry/main.tf
+++ container-registry/main.tf
@@ -3,4 +3,5 @@ resource "azurerm_container_registry" "container_registry" {
   resource_group_name      = "${var.resource_group_name}"
   location                 = "${var.resouce_group_location}"
   sku = "${var.sku_type}"
+  admin_enabled = true
 }
