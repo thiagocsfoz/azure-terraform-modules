968254ba95af859664d5c4d81a19061f7c889938
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-06-20 20:55:41 +1000

    container vars update

--- container-registry/vars.tf
+++ container-registry/vars.tf
@@ -1,16 +1,19 @@
 variable "registry_name" {
     name = "Name of the azure container registry"
+    type = "string"
 }
 
 variable "geo_locations" {
   name = "Locations to place the image"
-  default = ["East US", "West Europe"]
+  type= "list"
 }
 
 variable "resource_group_name" {
   description = "Name of the resource group"
+  type = "string"
 }
 
 variable "resouce_group_location" {
   description = "Location of the resource group"
+  type = "string"
 }
