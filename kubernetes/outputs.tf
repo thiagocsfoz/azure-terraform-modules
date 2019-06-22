03ce9c11b0d1860cd17fba15c91eff16f8adb289
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-06-22 20:37:50 +1000

    output cluster name and password

--- kubernetes/outputs.tf
+++ kubernetes/outputs.tf
@@ -2,6 +2,14 @@ output "id" {
   value = "${azurerm_kubernetes_cluster.kubernetes_cluster.id}"
 }
 
+output "cluster_username" {
+    value = "${azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.username}"
+}
+
+output "cluster_password" {
+    value = "${azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.password}"
+}
+
 output "kube_config" {
   value = "${azurerm_kubernetes_cluster.kubernetes_cluster.kube_config_raw}"
 }
