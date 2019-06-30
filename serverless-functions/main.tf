e8ea90976cd48a9e8d554898de13971a0610fe51
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-06-30 10:19:07 +1000

    change resource group name

--- serverless-functions/main.tf
+++ serverless-functions/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_app_service_plan" "app_service_plan" {
 resource "azurerm_function_app" "function_app" {
   name                      = "${var.function_app_name}"
   location                  = "${var.resouce_group_location}"
-  resource_group_name       = "${var.resouce_group_location}"
+  resource_group_name       = "${var.resource_group_name}"
   app_service_plan_id       = "${azurerm_app_service_plan.app_service_plan.id}"
   storage_connection_string = "${var.storage_connection_string}"
 }
