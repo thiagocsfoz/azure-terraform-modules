0d590c5c2c208c6e67a66f8091e1b9a125242e84
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-07-08 21:12:53 +1000

    function app runtime

--- serverless-functions/main.tf
+++ serverless-functions/main.tf
@@ -16,6 +16,7 @@ resource "azurerm_function_app" "function_app" {
   resource_group_name       = "${var.resource_group_name}"
   app_service_plan_id       = "${azurerm_app_service_plan.app_service_plan.id}"
   storage_connection_string = "${var.storage_connection_string}"
+  version = "~2"
 
   app_settings {
     HOST = "${var.host_name}"
@@ -26,9 +27,7 @@ resource "azurerm_function_app" "function_app" {
     WEBSITE_RUN_FROM_PACKAGE = "${var.run_from_package}"
     APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.app_insights.instrumentation_key}"
   }
-  site_config {
-    linux_fx_version = "java|1.8"
-  }
+
 }
 
 resource "azurerm_application_insights" "app_insights" {
