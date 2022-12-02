dfa3cb138bfd809c5a638c44a808f1882610b8c9
Thiago Chaves Santos <thiagocsfoz@gmail.com>
2022-12-01 22:43:02 -0300

    Update vars.tf

--- container-registry/vars.tf
+++ container-registry/vars.tf
@@ -1,19 +1,19 @@
 variable "registry_name" {
   description = "Name of the azure container registry"
-  type = "string"
+  type = string
 }
 
 variable "resource_group_name" {
   description = "Name of the resource group"
-  type = "string"
+  type = string
 }
 
 variable "resouce_group_location" {
   description = "Location of the resource group"
-  type = "string"
+  type = string
 }
 
 variable "sku_type" {
-  type = "string"
+  type = string
   default = "Basic"
 }
